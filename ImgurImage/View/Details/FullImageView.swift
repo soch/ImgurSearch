aaaaae4f5cbcd462b7d53b1b0e043e3517e5aa72
Amit Jain <amitjain16@gmail.com>
2024-10-07 15:39:06 -0700

    pinch to zoom

--- ImgurImage/View/Details/FullImageView.swift
+++ ImgurImage/View/Details/FullImageView.swift
@@ -11,12 +11,33 @@ import Kingfisher
 struct FullImageView: View {
     var imageUrl: String
     
+    @State private var currentZoom = 0.0
+    @State private var totalZoom = 1.0
+    
     var body: some View {
         if let url = URL(string: imageUrl) {
             KFImage(url)
                 .resizable()
                 .scaledToFit()
                 .padding()
+                .scaleEffect(currentZoom + totalZoom)
+                            .gesture(
+                                MagnifyGesture()
+                                    .onChanged { value in
+                                        currentZoom = value.magnification - 1
+                                    }
+                                    .onEnded { value in
+                                        totalZoom += currentZoom
+                                        currentZoom = 0
+                                    }
+                            )
+                            .accessibilityZoomAction { action in
+                                if action.direction == .zoomIn {
+                                    totalZoom += 1
+                                } else {
+                                    totalZoom -= 1
+                                }
+                            }
         }
     }
 }
